f681b15d92bb0ff2971050ba6a74798733f9611e
Alex Carmichael <carmichael.alexander@gmail.com>
2021-07-29 13:56:51 -0700

    skeletoned API endpoints

--- FishfryTours/Controllers/BoatsController.cs
+++ FishfryTours/Controllers/BoatsController.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Threading.Tasks;
 
 
-
 namespace FishfryTours.Controllers
 {
 	public class BoatsController : Controller
@@ -17,8 +16,17 @@ namespace FishfryTours.Controllers
 			_context = context;
 		}
 		private readonly DatabaseContext _context;
+
+		//just to test with Postman, etc.
 		[HttpGet]
-		public string Boats()
+		public string Ping() 
+		{
+			return "hello world";
+		}
+
+		//Get all boat entities in DB
+		[HttpGet]
+		public string Boats() 
 		{
 			try
 			{
@@ -32,9 +40,46 @@ namespace FishfryTours.Controllers
 			}
 		}
 		
-		[HttpGet]
-		public string Ping(){
-			return "hello world";
+		
+		//Create new boat entity in DB
+		[HttpPost]
+		public IActionResult CreateBoat()
+		{
+
+			return Ok();
+		}
+
+		//Destroy boat entity in DB
+		[HttpPost]
+		public IActionResult DeleteBoat(int id)
+		{
+
+			return Ok();
+		}
+
+		//Create new guide entity in DB
+		[HttpPost]
+		public IActionResult CreateGuide()
+		{
+
+			return Ok();
+		}
+
+		//update Guide info in DB (phone, email, etc.)
+		[HttpPost]
+		public IActionResult UpdateGuide(int id)
+		{
+
+			return Ok();
+		}
+
+
+		//Assign a guide to a boat
+		[HttpPost]
+		public IActionResult AssignGuideToBoat(int boatId, int guideId)
+		{
+
+			return Ok();
 		}
 
 	}
