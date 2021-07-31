0eef9dc2d5f9db906aa2d4cbd64ee8ec52eeef4c
Alex Carmichael <carmichael.alexander@gmail.com>
2021-07-30 22:29:34 -0700

    some drag & drop functionality

--- FishfryTours/wwwroot/js/Fishfry.jsx
+++ FishfryTours/wwwroot/js/Fishfry.jsx
@@ -1,7 +1,7 @@
 ﻿
 
 const statusLanes = {
-    Docked: [],
+    docked: [],
     outbound: [],
     inbound: [],
     maintenance: [],
@@ -24,33 +24,6 @@ class MainContent extends React.Component {
         xhr.send();
     }
 
-
-    onDragOver = e => {
-        e.preventDefault();
-    };
-
-    onDragStart = (e, id) => {
-        e.dataTransfer.setData("id", id);
-        console.log(id);
-    };
-
-    onDrop = (e, status) => {
-        let id = e.dataTransfer.getData("id");
-        let boats = this.state.data.filter(boat => {
-            if (boat.id == id) {
-                boat.category = status;
-                if (status == "indock") {
-                    boat.bgColor = "#e57373";
-                } else {
-                    boat.bgColor = "#9fa8da";
-                }
-            }
-            return boat;
-        });
-
-    //    this.setState({ ...this.state.data, data });
-    };
-
     handleAddNew = () => {
         let val = this.state.isClicked ? false : true;
   //      this.setState({ isClicked: val });
@@ -70,51 +43,20 @@ class MainContent extends React.Component {
     };
 
     render() {
-        //status swim lanes for boats
-        
+        //status swim lanes for boats     
         //assign boats to swim lanes based on satus
-        this.state.data.forEach(t => {
-            console.log(t);
-            if (statusLanes[t.Status])
-                statusLanes[t.Status].push(
-                    <div key={t.Id} onDragStart={e => this.onDragStart(e, t.Name)} draggable className="draggable" style={{ background: t.bgColor }}>
-                        {t.Name}
-                    </div>
-                );
-            else
-                statusLanes["Docked"].push(
-                    <div key={t.Id} onDragStart={e => this.onDragStart(e, t.Name)} draggable className="draggable" style={{ background: t.bgColor }}>
-                        {t.Name}
-                    </div>
-                );
-        });
+      
  
         return (
 
             /* main content here */
             <div className="mainContent">
                 <h1>Fishfry Tours</h1>
-                <BoatList data={this.state.data} />
 
-                <p className="header">Drag & Drop</p>
+                <p className="header">Drag & drop boats between swimlanes to set their status</p>
 
-                <DockedLane />
-
-                <div className="outboundLane" onDragOver={e => this.onDragOver(e)} onDrop={e => this.onDrop(e, "outbound")} >
-                    <span className="task-header">Outbound</span>
-                    {statusLanes.outbound}
-                        </div>
-                        <div className="inboundLane" onDragOver={e => this.onDragOver(e)} onDrop={e => this.onDrop(e, "inbound")} >
-                            <span className="task-header">Inbound</span>
-                            {statusLanes.inbound}
-                        </div>
-                        <div className="maintenanceLane" onDragOver={e => this.onDragOver(e)} onDrop={e => this.onDrop(e, "maintenance")} >
-                            <span className="task-header">Maintenance</span>
-                            {statusLanes.maintenance}
-                        </div>
-
-
-
+                <KanbanBoard data={this.state.data}/>
+               
             </div>
 
       
@@ -122,20 +64,111 @@ class MainContent extends React.Component {
         );
     }
 }
-
-class DockedLane extends React.Component {
+class KanbanBoard extends React.Component {
     render() {
+        this.props.data.forEach(t => {
+            if (statusLanes[t.Status])
+                statusLanes[t.Status].push(t);
+            else
+                statusLanes["docked"].push(t);
+        });
+        console.log(statusLanes);
+        return (
+            <div className="kanbanBoard">
+                <DockedLane data={statusLanes["docked"]} />
+                <OutboundLane data={statusLanes["outbound"]}/>
+                <InboundLane data={statusLanes["inbound"]}/>
+                <MaintenanceLane data={statusLanes["maintenance"]}/>
+            </div>
+        );
+    }
+}
+
+
+class Lane extends React.Component {
+    onDrop = (e, status) => {
+        let id = e.dataTransfer.getData("id");
+
+        console.log(status);
+        };
+
+        //    this.setState({ ...this.state.data, data });
+    onDragOver = e => {
+        e.preventDefault();
+    };
+    render() {
+        
+    }
+}
+
+class DockedLane extends Lane {
+    render() {
+        const boatNodes = this.props.data.map(boat => (
+            <Boat name={boat.Name} key={boat.Id} >
+                {boat.boatName}
+            </Boat>
+        ));
         return (
             <div className="dockedLane" onDragOver={e => this.onDragOver(e)} onDrop={e => this.onDrop(e, "indock")}>
-                <span className="task-header">Docked</span>
-                {statusLanes.Docked}
+                <span className="swimlaneHeader">Docked</span>
+                 
                 <span>
                     <a className="btn-floating btn">
                         <i className="material-icons" onClick={this.handleAddNew}>
                             add
                       </i>
+                      
                     </a>
+                    { boatNodes}
                 </span>
+            </div>
+        );
+    }
+}
+
+class OutboundLane extends React.Component {
+    onDrop = (e, status) => {
+        let id = e.dataTransfer.getData("id");
+        let boats = this.state.data.filter(boat => {
+            if (boat.id == id) {
+                boat.category = status;
+                if (status == "indock") {
+                    boat.bgColor = "#e57373";
+                } else {
+                    boat.bgColor = "#9fa8da";
+                }
+            }
+            return boat;
+        });
+    }
+    onDragOver = e => {
+        e.preventDefault();
+    };
+    render() {
+        return (
+            <div className="outboundLane" onDragOver={e => this.onDragOver(e)} onDrop={e => this.onDrop(e, "outbound")}>
+                <span className="swimlaneHeader">Outbound</span>
+                {statusLanes.outbound}
+            </div>
+        );
+    }
+}
+class InboundLane extends React.Component {
+    render() {
+        return (
+            <div className="inboundLane" onDragOver={e => this.onDragOver(e)} onDrop={e => this.onDrop(e, "inbound")}>
+                <span className="swimlaneHeader">Inbound</span>
+                {statusLanes.inbound}
+            </div>
+        );
+    }
+}
+class MaintenanceLane extends React.Component {
+    render() {
+        return (
+            <div className="maintenanceLane" onDragOver={e => this.onDragOver(e)} onDrop={e => this.onDrop(e, "maintenance")}>
+                <span className="swimlaneHeader">Maintenance</span>
+                {statusLanes.maintenance}
             </div>
         );
     }
@@ -149,22 +182,23 @@ class BoatList extends React.Component {
             </Boat>
         ));
         return <div className="boatList">{boatNodes}</div>;
-        //return (
-        //    <div className="commentList">
-        //        <h2>Boats in the Fishfry Tours fleet</h2>
-        //        <Boat name="Daniel Lo Nigro">
-                    
-        //        </Boat>
-        //    </div>
-        //);
     }
 
 }
 
+/*boat draggable card */
 class Boat extends React.Component {
+   
+    onDragStart = (e, id) => {
+        e.dataTransfer.setData("id", id);
+        console.log(id);
+    };
+    onDragOver = e => {
+        e.preventDefault();
+    };
     render() {
         return (
-            <div className="boat">
+            <div className="boat" onDragStart={e => this.onDragStart(e, this.Name)} draggable className="draggable" style={{ background: this.bgColor }}>
                 <h3 className="boatName">{this.props.name}</h3>
                 {this.props.children}
             </div>
